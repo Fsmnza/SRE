6bf8adbbb5666aee7949e2fb5529c360fc574eb5
Fsmnza <140049580+Fsmnza@users.noreply.github.com>
2024-04-14 15:35:09 +0500

    first commit

---
+++ src/test/java/malika/application/logstashsenderapplication/LogstashSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package malika.application.logstashsenderapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogstashSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
